0fe135c828e135788a8eb832d6e6d9e961858729
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2023-11-19 18:01:29 +0530

    Add static folder

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Falsk Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello world!"
+
+#Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your Name", validators=[DataRequired()])
+    submit = SubmitField("submit")
+
 
 # Create a route decorator
 @app.route('/')
-
-#def index():
-#    return "<h1>Hello world!<h1>"
 
 def index():
     first_name = "Ravindu"
@@ -36,5 +43,23 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+#create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html", 
+                           name = name,
+                           form = form)
+
+
+
 
 
